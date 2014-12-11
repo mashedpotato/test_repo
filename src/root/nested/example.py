1a5ddfeaa7a11a6cccd6d9a0e6b5e0202d63e623
mashedpotato <mail2lasse@swipnet.se>
2014-12-12 00:18:39 +0100

    hejsan mer commit

---
+++ src/root/nested/example.py
@@ -0,0 +1,22 @@
+'''
+Created on 10 nov 2014
+
+@author: lasse
+'''
+if __name__ == '__main__':
+    print 'Hello World'
+    a=15
+    while a>=11:
+        print(a)
+        a-=1
+    print(type(a))
+    myDict={'*':34,'sdf':2}
+    print(myDict['*'])
+    print(myDict)
+    
+    d=82.3476585
+    print'd is:', d
+    print'd is: %.3f' %d
+    print'd is: %4.1f' %d
+    print'd is: %.3s' %d
+    
